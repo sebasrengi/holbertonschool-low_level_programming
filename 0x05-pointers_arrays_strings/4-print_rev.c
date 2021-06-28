c8e4839a6ef3c3bd98a7fc7b18151db86aa7e88e
sebasrengi <sebas.rengi@hotmailcom>
2021-06-28 16:56:56 -0700

    print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string in stdout in reverse.
+ * @s: The string to be printed.
+ */
+void print_rev(char *s)
+{
+while (*s)
+{
+_putchar (*s++);
+}
+while (*s)
+{
+_putchar(*s--);
+}
+_putchar('\n');
+}
