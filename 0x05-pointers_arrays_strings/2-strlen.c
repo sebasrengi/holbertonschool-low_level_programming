775534bcee692f45357859f2944c48646126914a
sebasrengi <sebas.rengi@hotmailcom>
2021-06-28 12:24:16 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - give the length of a string
+ * @s: the string
+ *
+ * Return: the length of a string
+ */
+int _strlen(char *s);
+{
+int len = strlen(s);
+return (0);
+}
