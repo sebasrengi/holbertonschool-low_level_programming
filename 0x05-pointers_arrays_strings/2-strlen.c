ca6858eaf82558f7439af141fed6aca8134f8005
sebasrengi <sebas.rengi@hotmailcom>
2021-06-28 15:09:14 -0700

    s

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,13 @@
 #include "holberton.h"
-#include <stdio.h>
 /**
- * _strlen - give the length of a string
- * @s: the string
- *
- * Return: the length of a string
- */
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
 int _strlen(char *s)
 {
 	int c;
-	s[10] = "Holberton!";
 
 	for (c = 0; s[c]; s++)
 	{
