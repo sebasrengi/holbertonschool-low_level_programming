df78a116271692966c55c0fbfa328e5655942f3b
sebasrengi <sebas.rengi@hotmailcom>
2021-07-07 16:51:11 -0700

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,13 +6,13 @@
  * Return: void
  */
 void _puts_recursion(char *s)
-{if(*s == '\0')
-    { putchar('\n');
-      
-    }
-  else
-    {
-  _putchar(*s);
-  _puts_recursion(s + 1);
-  }
+{
+if (*s == '\0')
+{putchar('\n');
+}
+else
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
 }
