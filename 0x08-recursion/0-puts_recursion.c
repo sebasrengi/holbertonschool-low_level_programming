fc2f5456034531727d04ca06ab7c3692979de1de
sebasrengi <sebas.rengi@hotmailcom>
2021-07-07 16:44:02 -0700

    puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,8 +8,11 @@
 void _puts_recursion(char *s)
 {if(*s == '\0')
     { putchar('\n');
-      return (1);
+      
     }
-  *s++;
-  _puts_recursion(*s);
+  else
+    {
+  _putchar(*s);
+  _puts_recursion(*s + 1);
+  }
 }
