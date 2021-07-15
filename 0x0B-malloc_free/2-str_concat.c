bd5c2cf7d48255105119742fa0e21da338d8407c
sebasrengi <sebas.rengi@hotmailcom>
2021-07-14 17:38:38 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,40 +8,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-  int i = 0, j = 0, suma, cont = 0, x = 0;
-  char *array;
-
-    if (s1 == NULL)
-    {
-        s1 = "";
-    }
-    if (s2 == NULL)
-    {
-        s2 = "";
-    }
-    while(s1[i])
-    i++;
-
-    while(s2[j])
-    j++;
-
-    suma = i + j;
-    array = malloc((sizeof(char) * suma) + 1);
-
-    if (array == NULL)
-        return (NULL);
-    for (; cont < suma; cont++)
-    {
-        if (cont <= i)
-            array[cont] = s1[cont];
-
-        if (cont >= i)
-        {
-            array[cont] = s2[x];
-            x++;
-        }
-    }
-
-    array[cont] = '\0';
-    return (array);
+int i = 0, j = 0, suma, cont = 0, x = 0;
+char *array;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+while(s1[i])
+i++;
+while(s2[j])
+j++;
+suma = i + j;
+array = malloc((sizeof(char) * suma) + 1);
+if (array == NULL)
+return (NULL);
+for (; cont < suma; cont++)
+{
+if (cont <= i)
+array[cont] = s1[cont];
+if (cont >= i)
+{
+array[cont] = s2[x];
+x++;
+}
+}
+array[cont] = '\0';
+return (array);
 }
