7f0e0d648c44aa751aa6289c747db5e2483e239e
sebasrengi <sebas.rengi@hotmailcom>
2021-07-19 16:55:13 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef 0-object_like_macro.h
+#define 0-object_like_macro.h
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+#define SIZE 1024;
+
+#endif
